e4e025dc06b150a94f2a156ed046a53f5ce78a14
robojones <jonathan.scholz-weil@t-online.de>
2017-08-28 17:24:28 +0200

    add tests

--- test/index.js
+++ test/index.js
@@ -3,5 +3,45 @@ const assert = require('assert')
 describe('command-test', function () {
   const command = require('..')
 
-  it('should work')
+  const validCommand = 'node -e \'console.log("hi")\''
+  const invalidCommand = 'node -e \'throw new Error("WROONG!!!")\''
+
+  describe('command(cmd, exitCode)', function () {
+    it('should always resolve to a command object', async function () {
+      await command(validCommand)
+      await command(invalidCommand)
+    })
+  })
+
+  describe('command.works(cmd)', function () {
+    it('should not throw if the command exits with exit code == 0', async function () {
+      await command.works(validCommand)
+    })
+
+    it('should throw if the command exits with an exit code != 0', async function () {
+      try {
+        await command.works(invalidCommand)
+      } catch (err) {
+        return
+      }
+
+      throw new Error('No error was thrown.')
+    })
+  })
+
+  describe('command.fails(cmd)', function () {
+    it('should not throw if the command exits with exit code != 0', async function () {
+      await command.fails(invalidCommand)
+    })
+
+    it('should throw if the command exits with an exit code == 0', async function () {
+      try {
+        await command.fails(validCommand)
+      } catch (err) {
+        return
+      }
+
+      throw new Error('No error was thrown.')
+    })
+  })
 })
